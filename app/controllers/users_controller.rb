2b05a24e27b05ab8fc7fd9a662ca290d61c94d37
Prima Aulia Gusta <primAulia@gmail.com>
2017-11-06 14:10:15 +0800

    latest routes and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def index
+    # find all users
+    @all_user = User.all
+
+  end
+
+  def show
+    # find one user by name
+    @user = User.find_by(name: params[:name])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/users', to: 'users#index' # READ all users
+  # get '/users/:name', to: 'users#show' # READ one user by name
+
+  # instead of writing the last two lines, we can run it in one line
+  resources :users, only: [:index, :show]
 end
