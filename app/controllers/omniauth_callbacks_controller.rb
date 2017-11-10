cb150acdcfc90a7c354417a597bfb0f561262c6c
Prima Aulia Gusta <primAulia@gmail.com>
2017-11-10 16:11:49 +0800

    remove the namespace

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # after login thru fb
 
